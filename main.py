e6dd6f9e60b5efe90810f69ade00341c5f9188c6
Ermuun Enkhjavkhlan <62606691+ermuunenkh@users.noreply.github.com>
2023-06-10 19:35:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "https://deploy-test-5032f.web.app",  # replace with your frontend's URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/data")
 async def read_data():
